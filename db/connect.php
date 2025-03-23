cd9c57d872acabef6edecf32db0dffd644d13552
soul redouane <101919227+SOULREDOUANE@users.noreply.github.com>
2025-03-23 19:25:10 +0000

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,24 @@
 <?php
+$servername = "mysql"; // Use the Docker service name
+$username = "root"; // Replace with your MySQL user
+$password = ""; // Replace with your MySQL password
+$db = "eventsite"; // Replace with your database name
 
-$servername = "localhost";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
+<!-- <?php
+
+$servername = "129.151.253.241";
 $username = "root";
-$password = "";
-$db = "eventmgmt";
+$password = "your_password";
+$db = "eventsite";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
@@ -14,4 +29,4 @@ if (!$con) {
 }
 
 
-?>
+?> -->
